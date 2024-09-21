191d49ea50bb8474c9eeff1c49f98518e81d6301
Mxiabi1996 <michelle_montoya96@outlook.com>
2024-09-21 18:02:25 -0500

    Agregando boton

--- sem02.py
+++ sem02.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 #* inicializacion de variables
-usuario = []
+usuarios = []
 
 #* funcion agregar un usurario
 def agregar_usuario(nombre):
-    usuario.append(nombre)
-    st.success(f"Usuario {usuario} agregado",icon="✅")
+    usuarios.append(nombre)
+    st.success(f"Usuario {nombre} agregado",icon="✅")
 
 #* Mostrar usuarios
 def mostrar_usuario():
@@ -22,12 +22,15 @@ st.title("Gestion de usuarios")
 
 #* Opciones
 opcion = st.selectbox("selecciona una opcion:" , "Agregar usuarios","Mostrar usuarios")
+nombre = st.text_input('nombre del usuario')
+
 if opcion == "Agregar usuarios":
-    nombre = st.text_input('nombre del usuario')
+    
     if st.button("Agregar", type='primary'):
         if nombre:
             agregar_usuario(nombre)
         else:
             st.error("No puede estar vacio")
 elif opcion == "Mostrar usuarios":
-    mostrar_usuario ()
+    if st.button("Mostrar usuarios",type="primary"):
+        mostrar_usuario()
