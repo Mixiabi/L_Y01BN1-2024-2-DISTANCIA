f982c06b0abda75276e5e879b45497e735047956
Mxiabi1996 <michelle_montoya96@outlook.com>
2024-10-21 21:34:39 -0500

    Modificando condicional

--- sem06.py
+++ sem06.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import streamlit.components.v1 as components
+
 st.subheader("Ejercicio 1: Imprimir 'Hola Mundo' 10 veces")
 if st.button("Ejecutar Ejercicio 1"):
     for i in range(10):
@@ -9,28 +11,76 @@ if st.button("Ejecutar Ejercicio 2"):
     for i in range(1,11):
         st.write(i)
         
-st.subheader("Ejercicio 1: Imprimir 'Hola Mundo' 10 veces")
+st.subheader("Ejercicio 3: Imprimir 'Hola Mundo' 10 veces")
 num = st.number_input("Ingrese un numero para ver su tabla de mulitplicar del 1 al 12", min_value=1)
 
 if st.button("Ejecutar Ejercicio 3"):
     for i in range(1,13):
         st.write(f'{num} * {i} = {num * i}')
 
-st.subheader('Ejercicio 4: Calcula la media y compara con 10')
+#? *************************************************************************************
+#?                              E J E R C I C I O   # 4
+#? *************************************************************************************
+if 'lista_numeros' not in st.session_state:
+    st.session_state.lista_numeros = []
 
-#Ejercicio 4: Calcular la media y comparar con 10
-st.subheader("Ejercicio 1: Comparar 10 números con el valor 10")
-numeros_ej1 = st.text_input("Ingresa 10 números separados por comas:", "12, 7, 15, 10, 20, 5, 3, 8, 13, 25")
+def set_autofocus():
+    autofocus_script = """
+    <script>
+    document.getElementById("num_input").focus();
+    </script>
+    """
+    components.html(autofocus_script)
 
-if st.button("Ejecutar Ejercicio 1"):
-    # Convertir la cadena de entrada a una lista de números
-    lista_numeros = [int(num) for num in numeros_ej1.split(",")]
-    media = sum(lista_numeros) / len(lista_numeros)
-    mayores = len([num for num in lista_numeros if num > 10])
-    iguales = len([num for num in lista_numeros if num == 10])
-    menores = len([num for num in lista_numeros if num < 10])
+def set_reinicio():
+    if st.button("Reiniciar"):
+        st.session_state.lista_numeros = []
+        set_autofocus()
 
+def set_calculo():
+    media = sum(st.session_state.lista_numeros) / len(st.session_state.lista_numeros)
+        
+    mayores = len([num for num in st.session_state.lista_numeros if num > 10])
+    iguales = len([num for num in st.session_state.lista_numeros if num == 10])
+    menores = len([num for num in st.session_state.lista_numeros if num < 10])
+        
     st.write(f"La media es: {media}")
     st.write(f"Mayores que 10: {mayores}")
     st.write(f"Iguales a 10: {iguales}")
     st.write(f"Menores que 10: {menores}")
+
+# CSS personalizado para ajustar márgenes
+st.markdown("""
+    <style>
+    .stTextInput > div {
+        margin-bottom: 0px;
+    }
+    .stButton {
+        margin-top: -10px;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+with st.form(key='my_form', clear_on_submit=True):
+    numero = st.text_input("Ingresa un número y presiona Enter", key="num_input")
+    submitted = st.form_submit_button("Agregar")
+    set_autofocus()
+
+if submitted and numero:
+    
+    if len(st.session_state.lista_numeros) < 10:
+        st.write(f"Números ingresados: {st.session_state.lista_numeros}")
+        try:
+            numero = int(numero)
+            st.session_state.lista_numeros.append(numero)
+            set_calculo()
+            
+        except ValueError:
+            st.write("Por favor, ingresa un número válido.")
+        
+            
+    else:
+        st.write("Ya has ingresado 10 números, no puedes agregar más.")
+        st.write(f"Números ingresados: {st.session_state.lista_numeros}")
+
+set_reinicio()
